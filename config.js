c6a8b3665c5ab3b184f815e83d2147336745ed60
JimmyCiculs <96069122+Ciculs@users.noreply.github.com>
2022-03-18 19:25:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     app: {
         px: '!',
-        token: 'OTUwNzgwODYwOTQzMTk2MTky.Yid5rQ.7siIlYZCnVOhhuiR8uQq-RWVLYc',
+        token: 'OTUwNzgwODYwOTQzMTk2MTky.Yid5rQ.mx-2RPlDl0payw7wiMFVrSf7kmA',
         playing: 'by Ciculs ❤️'
     },
 
